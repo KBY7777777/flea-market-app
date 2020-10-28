6b345b057967ad19a2c44b9986ff30964e2a90ba
KBY7777777 <koba.yu.7777777@gmail.com>
2020-10-28 21:56:36 +0900

    The codes for User Login Page have been created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   root 'items#index'
+
+  resources :users do    #ユーザー情報関連の「users」controllerに対応
+    collection do
+      get 'index'    #必要時に使用
+      put 'index'
+      get 'login'    #ログインページ
+      put 'login'
+    end
+  end
+
 end
