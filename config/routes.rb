da1d542d8ad9e506ab9347ff66639a1322314c0e
sd0821 <68905745+sd0821@users.noreply.github.com>
2020-11-03 21:19:37 +0900

    User registration implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+}
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root 'items#index'
-
-  resources :items do    #商品情報関連の「items」controllerに対応
-    collection do
-      get 'purchase'    #商品購入確認ページ
-      get 'sell'    #商品出品ページ
-      get 'item_details'    #商品詳細ページ
-    end
-  end
-
-  resources :users do    #ユーザー情報関連の「users」controllerに対応
-    collection do
-      get 'login'    #ログインページ
-    end
-  end
 
 end
