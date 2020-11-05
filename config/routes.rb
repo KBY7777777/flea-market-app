770609164bf105321fc597d763bfe6afe287dcb0
KBY7777777 <koba.yu.7777777@gmail.com>
2020-11-05 21:28:48 +0900

    The codes for User Mypage Top View have been created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-}
+  }
+
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+
   root 'items#index'
+
+  resources :mypages do
+    collection do
+      get 'index'
+    end
+  end
 
 end
