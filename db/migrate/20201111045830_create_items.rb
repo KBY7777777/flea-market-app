cc3a84e0185127f8d87ddb2c512563e02534350c
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-11-11 14:16:00 +0900

    itemカラム設定

---
+++ db/migrate/20201111045830_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user,           null: false,foreign_key:true
+      t.string :item_name,          null: false
+      t.text :item_text,            null: false
+      t.integer :category_id,       null: false
+      t.string :brand
+      t.integer :item_status,       null: false
+      t.integer :delivery_charge,   null: false
+      t.integer :delivery_area,     null: false
+      t.integer :delivery_day,      null: false
+      t.integer :price,             null: false   
+
+      t.timestamps
+    end
+  end
+end
