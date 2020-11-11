d3b19be618f37d94014a7ae02eac2239532830cb
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-11-11 18:38:26 +0900

    Category コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-
+  before_action :set_category, only: [:new, :create, :sell ]
+  
   def index
   end
 
@@ -7,9 +8,43 @@ class ItemsController < ApplicationController
   end
 
   def sell
+    @item = Item.new
   end
 
   def item_details
   end
+  
+
+  def get_category_children
+    @category_children = Category.find(params[:parent_name]).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+  # def new
+  #   @item = Item.new
+  # end
+
+  def create
+    @item = Item.new(item_params)
+    if  @item.save
+      redirect_to root_path 
+    else
+      render :sell
+    end
+  end
+
+  private
+
+  def set_category  
+    @category_parent_array = Category.where(ancestry: nil)
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name,:item_text,:item_status,:delivery_charge,:delivery_area,:delivery_day,:price, :category_id, :brand).merge(user_id: current_user.id)
+  end
 
 end
+
