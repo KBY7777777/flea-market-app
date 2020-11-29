5828361696aa90176c7a165d584b0a679e9a8b18
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-11-29 11:27:52 +0900

    edit対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,9 +18,9 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :item_images,dependent: :destroy
-  accepts_nested_attributes_for :item_images
+  accepts_nested_attributes_for :item_images, allow_destroy: true
   validates :item_images, presence: true
-  validates_associated :item_images
+  #validates_associated :item_images
   # has_one :purchase_management
 
   extend ActiveHash::Associations::ActiveRecordExtensions
