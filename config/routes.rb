edd87da828be9b44ae24075edeb61ff4f900be5d
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-12-11 00:03:40 +0900

    商品一覧作成

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :items do
 
     collection do
+      get 'display'
       get 'sell'
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
